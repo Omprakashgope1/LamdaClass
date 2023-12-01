d8d8269c608fc0f82357086d04ce2d161c4df8c5
Om Prakash Gope <bindass.omi.1997@gmail.com>
2023-12-01 12:46:07 +0530

    first

---
+++ LamdaExpression/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Data.Common;
+
+namespace LamdaExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write the operation number you want to perform");
+            int val = Convert.ToInt32(Console.ReadLine());
+            List<Student> students = new List<Student>();
+            students.Add(new Student(1, "Sachin", "145165", "Nammkum", 16));
+            students.Add(new Student(2, "Sachin", "145165", "Nammkum", 16));
+            students.Add(new Student(3, "Sachin", "145165", "Nammkum", 16));
+            students.Add(new Student(4, "Kapil", "52513231", "Ranchi", 21));
+            students.Add(new Student(5, "Wakil", "52513231", "Ranchi", 21));
+            students.Add(new Student(6, "Tom", "52513231", "Ranchi", 14));
+            students.Add(new Student(7, "Charlie", "52513231", "Delhi", 12));
+            students.Add(new Student(8, "Chandan", "52513231", "Bangalore", 5));
+            switch (val)
+            {
+                case 1:
+                    foreach(Student student in students) 
+                    {
+                        Console.WriteLine(student);
+                    }
+                    break;
+                case 2:
+                    List<Student> AgeLimitList = students.Where(a => a.Age >= 12 && a.Age <= 16).ToList();
+                    //Console.WriteLine(ageLimitList.Count);
+                    foreach (Student student in AgeLimitList)
+                    {
+                        Console.WriteLine(student);
+                    }
+                    break;
+                case 3:
+                    List<Student> DesendingList = (from stud in students orderby stud.Age select stud).ToList();
+                    foreach (Student student in DesendingList)
+                    {
+                        Console.WriteLine(student);
+                    }
+                    break;
+                case 4:
+                    var GroupBy = (from stud in students group stud by stud.Address into newgroup select newgroup);
+                    foreach(var addresslist in GroupBy)
+                    {
+                        Console.WriteLine("Location : " + addresslist.Key);
+                        foreach(Student studen in addresslist)
+                        {
+                            Console.WriteLine(studen);
+                        }
+                        Console.WriteLine();
+                    }
+                    break;
+                case 5:
+                    var OnlyThreeList = (from stud in students select stud).Take(3).ToList();
+                    foreach(Student student in OnlyThreeList)
+                    {
+                        Console.WriteLine(student);
+                        Console.WriteLine();
+                    }
+                    break;
+                case 6:
+                    Console.WriteLine("Name the person you are looking for : ");
+                    String find = Console.ReadLine();
+                    var studList = students.FindAll(a => a.Name == find);
+                    foreach(var stud in studList)
+                    {
+                        Console.WriteLine(stud);
+                        Console.WriteLine();
+                    }
+                    if (studList.Count == 0) Console.WriteLine("No match found");
+                    break;
+
+            }
+        }
+    }
+}
---
+++ LamdaExpression/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LamdaExpression
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNum { get; set; }
+        public string Address {  get; set; }
+        public int Age {  get; set; }   
+       
+        public Student(int Id,string Name,string PhoneNum,string Address,int Age)
+        {
+            this.Age = Age;
+            this.Id = Id;   
+            this.Name = Name;
+            this.PhoneNum = PhoneNum;
+            this.Address = Address;
+        }
+        public override string ToString() 
+        {
+            return "Id of the student : " + this.Id
+                + "\nName of the student : " + this.Name +
+                "\nAge of the student :" + this.Age +
+                "\nPhone number of the student :" + this.PhoneNum +
+                "\nAddress of the student : " + this.Address +
+                "\nPh. number of the student :" + this.PhoneNum;
+        }
+        
+    }
+}
